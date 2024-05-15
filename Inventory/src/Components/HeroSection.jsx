3d1cf1802c227b118b4c294fe4a918f9609c4b4d
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-05-15 12:37:16 +0100

    Hero section started

--- Inventory/src/Components/HeroSection.jsx
+++ Inventory/src/Components/HeroSection.jsx
@@ -1,10 +1,19 @@
-
+import bgImage from '../assets/Images/woman-business.png'
 
 function HeroSection() {
   return (
-    <>
-     <h2>Hero Section</h2> 
-    </>
+  <>
+    <div className="w-[1440px] h-[680px] flex justify-start items-center relative">
+    <div className="absolute inset-0 bg-gradient-to-l from-white via-lime-950 to-slate-950 backdrop-blur-sm"></div>
+
+    <img className="absolute inset-0 object-cover w-full h-full" src={bgImage}  alt="Background"  />  
+    </div>
+    
+      
+   
+   
+
+  </>
   )
 }
 
